5d8fedbba1fe6cd238b445f7d87fa2b3fd2c09ec
Mac-Yang <874681044@qq.com>
2022-03-20 02:38:16 +0800

    [recipe] cnc

--- recipes/CnCeleb/make_rirs.py
+++ recipes/CnCeleb/make_rirs.py
@@ -20,7 +20,7 @@ pn_dir = sys.argv[1]
 
 pn_dir=pathlib.Path(pn_dir)
 
-with open(pn_dir + '/noise_list', 'w') as f:
+with open(str(pn_dir) + '/noise_list', 'w') as f:
     for wav in pn_dir.glob('*.wav'):
         wav_id = str(wav).split('/')[-1].split('.')[0]
         f.write('--noise-id ' + wav_id + '--noise-type point-source --bg-fg-type foreground RIRS_NOISES/pointsource_noises/' + wav_id + '\n')
