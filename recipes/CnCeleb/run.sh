362b8fd0e99c724679a9a1773f805f1eaaebe9b2
william-asus <874681044@qq.com>
2022-03-21 01:23:07 +0800

    [train] cnc ecapa epoch20

--- recipes/CnCeleb/run.sh
+++ recipes/CnCeleb/run.sh
@@ -17,13 +17,22 @@ done
 
 if [ $stage -le 0 ]; then
 
+  CUDA_VISIBLE_DEVICES=0,1,2 python -m torch.distributed.launch --nproc_per_node=2 train_speaker_embeddings.py SpeakerRec/hparams/train_ecapa_tdnn.yaml --distributed_launch --distributed_backend='nccl'
+
+#  CUDA_VISIBLE_DEVICES=0 python speaker_verification_cosine.py SpeakerRec/hparams/verification_ecapa.yaml
+  exit
+fi
+
+if [ $stage -le 5 ]; then
+
 #  CUDA_VISIBLE_DEVICES=0,1,2 python -m torch.distributed.launch --nproc_per_node=3 train_speaker_embeddings.py SpeakerRec/hparams/train_ecapa_tdnn.yaml --distributed_launch --distributed_backend='nccl'
 
   CUDA_VISIBLE_DEVICES=0 python speaker_verification_cosine.py SpeakerRec/hparams/verification_ecapa.yaml
   exit
 fi
 
-if [ $stage -le 1 ]; then
+
+if [ $stage -le 10 ]; then
 
   CUDA_VISIBLE_DEVICES=1,2 python -m torch.distributed.launch --nproc_per_node=2 train_speaker_embeddings.py SpeakerRec/hparams/train_x_vectors.yaml --distributed_launch --distributed_backend='nccl'
 
