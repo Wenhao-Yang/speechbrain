dbc8bbb43de8ecc1f0ef3bbbf0e7a7d4e1fb708f
Mac-Yang <874681044@qq.com>
2022-03-23 18:16:52 +0800

    [train] rand seed xvector double

--- recipes/CnCeleb/cnceleb_prepare.py
+++ recipes/CnCeleb/cnceleb_prepare.py
@@ -365,10 +365,10 @@ def _get_chunks(seg_dur, audio_id, audio_duration):
     for i in range(num_chunks):
         chunk_lst.add(audio_id + "_" + str(i * seg_dur) + "_" + str(i * seg_dur + seg_dur))
 
-    if audio_duration > seg_dur:
-        for i in range(num_chunks):
-            start = np.random.randint(0, int((audio_duration - seg_dur) * SAMPLERATE)) / SAMPLERATE
-            chunk_lst.add(audio_id + "_" + str(start) + "_" + str(start + seg_dur))
+    # if audio_duration > seg_dur:
+    #     for i in range(num_chunks):
+    #         start = np.random.randint(0, int((audio_duration - seg_dur) * SAMPLERATE)) / SAMPLERATE
+    #         chunk_lst.add(audio_id + "_" + str(start) + "_" + str(start + seg_dur))
 
     return list(chunk_lst)
 
