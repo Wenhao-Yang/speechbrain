47171ca76eb9099b7cfe5014e2efca2b277d43ba
Mac-Yang <874681044@qq.com>
2022-03-30 21:41:24 +0800

    [model] ecapa sinc

--- recipes/CnCeleb1/run.sh
+++ recipes/CnCeleb1/run.sh
@@ -6,7 +6,7 @@
 # time: 2022/3/20 01:05
 # Description: 
 
-stage=0
+stage=10
 
 waited=0
 while [ $(ps 17765 | wc -l) -eq 2 ]; do
@@ -30,4 +30,16 @@ if [ $stage -le 1 ]; then
 
   CUDA_VISIBLE_DEVICES=2,4 python -m torch.distributed.launch --nproc_per_node=2 train_speaker_embeddings.py SpeakerRec/hparams/train_x_vectors.yaml --distributed_launch --distributed_backend='nccl'
 
+fi
+
+
+if [ $stage -le 10 ]; then
+
+  CUDA_VISIBLE_DEVICES=1,2 python -m torch.distributed.launch --nproc_per_node=2 train_speaker_embeddings_end2end.py SpeakerRec/hparams/train_sinc_ecapa_tdnn.yaml --distributed_launch --distributed_backend='nccl'
+
+
+#  CUDA_VISIBLE_DEVICES=0 python speaker_verification_cosine.py SpeakerRec/hparams/verification_ecapa.yaml
+#  CUDA_VISIBLE_DEVICES=0 python speaker_verification_plda.py SpeakerRec/hparams/verification_plda_ecapa.yaml
+
+  exit
 fi
