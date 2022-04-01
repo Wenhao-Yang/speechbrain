edb1b9d7f05496ef2a4bdec7b7a23c38d1fa6b59
Mac-Yang <874681044@qq.com>
2022-04-01 15:19:02 +0800

    [model] ecapa sinc

--- recipes/CnCeleb1/speaker_verification_cosine.py
+++ recipes/CnCeleb1/speaker_verification_cosine.py
@@ -418,19 +418,23 @@ if __name__ == "__main__":
         overrides=overrides,
     )
 
-    # Prepare data from dev of Voxceleb1
-    # prepare_cnceleb(
-    #     data_folder=params["data_folder"],
-    #     save_folder=params["save_folder"],
-    #     verification_pairs_file=veri_file_path,
-    #     splits=["train", "dev", "test"],
-    #     split_ratio=params["split_ratio"],
-    #     seg_dur=params["sentence_len"],
-    #     source=params["voxceleb_source"]
-    #     if "voxceleb_source" in params
-    #     else None,
-    #     skip_prep=params["skip_prep"],
-    # )
+    Prepare
+    data
+    from dev of
+
+    Voxceleb1
+    prepare_cnceleb(
+        data_folder=params["data_folder"],
+        save_folder=params["save_folder"],
+        verification_pairs_file=veri_file_path,
+        splits=["train", "dev", "test"],
+        split_ratio=params["split_ratio"],
+        seg_dur=params["sentence_len"],
+        source=params["voxceleb_source"]
+        if "voxceleb_source" in params
+        else None,
+        skip_prep=params["skip_prep"],
+    )
 
     # here we create the datasets objects as well as tokenization and encoding
     train_dataloader, enrol_dataloader, test_dataloader = dataio_prep(params)
