c7f2970b12108166b4dfd4ae93848d445478b3d6
Mac-Yang <874681044@qq.com>
2022-04-26 17:05:51 +0800

    [train] cn12 resnet

--- recipes/CnCeleb1/train_speaker_embeddings.py
+++ recipes/CnCeleb1/train_speaker_embeddings.py
@@ -128,7 +128,8 @@ class SpeakerBrain(sb.core.Brain):
 
         # Concatenate labels (due to data augmentation)
         if stage == sb.Stage.TRAIN:
-            if len(self.hparams.augment_spk_pipeline) > 0:
+
+            if hasattr(self.hparams, "augment_spk_pipeline") and len(self.hparams.augment_spk_pipeline) > 0:
                 spks_aug_tot = [spkid]
                 for count, augment in enumerate(self.hparams.augment_spk_pipeline):
                     if isinstance(augment, sb.lobes.augment.TimeDomainSpecAugment):
