103b6e34b77b2d87c8028f9ec4565ec60543ee53
Mac-Yang <874681044@qq.com>
2022-05-13 22:20:41 +0800

    [train] early stopping

--- recipes/CnCeleb1/speaker_verification_cosine.py
+++ recipes/CnCeleb1/speaker_verification_cosine.py
@@ -76,7 +76,7 @@ def compute_embedding_loop(data_loader):
             if not found:
                 continue
 
-            pdb.set_trace()
+            # pdb.set_trace()
 
             if 'test_input' in params and params['test_input'] == 'fix':
                 input_wavs = []
@@ -87,8 +87,8 @@ def compute_embedding_loop(data_loader):
                 for i, wav in enumerate(wavs):
                     num_chunk = int(lens[i] * wavs.shape[1] / chunk_size)
                     for j in range(num_chunk):
-                        start = j * chunk_size
-                        end = start + chunk_size
+                        start = int(j * chunk_size)
+                        end = int(start + chunk_size)
                         if end > int(lens[i] * wavs.shape[1]):
                             start = int(lens[i] * wavs.shape[1]) - chunk_size
                             end = int(lens[i] * wavs.shape[1])
