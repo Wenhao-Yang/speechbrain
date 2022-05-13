738adf7724ffff347c4a3e32e1c5e411e7b9a38f
Mac-Yang <874681044@qq.com>
2022-05-14 00:47:30 +0800

    [train] early stopping

--- recipes/CnCeleb1/run.sh
+++ recipes/CnCeleb1/run.sh
@@ -9,7 +9,7 @@
 stage=30
 
 waited=0
-while [ $(ps 17765 | wc -l) -eq 2 ]; do
+while [ $(ps 217457 | wc -l) -eq 2 ]; do
   sleep 60
   waited=$(expr $waited + 1)
   echo -en "\033[1;4;31m Having waited for ${waited} minutes!\033[0m\r"
@@ -57,10 +57,10 @@ fi
 
 if [ $stage -le 30 ]; then
 
-#  CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --master_port='29504' --nproc_per_node=2 train_speaker_embeddings.py SpeakerRec/hparams/train_resnet.yaml --distributed_launch --distributed_backend='nccl'
+  CUDA_VISIBLE_DEVICES=0,3 python -m torch.distributed.launch --master_port='29505' --nproc_per_node=2 train_speaker_embeddings.py SpeakerRec/hparams/train_resnet.yaml --distributed_launch --distributed_backend='nccl'
 
 
-  CUDA_VISIBLE_DEVICES=3 python speaker_verification_cosine.py SpeakerRec/hparams/verification_res2net.yaml
+#  CUDA_VISIBLE_DEVICES=3 python speaker_verification_cosine.py SpeakerRec/hparams/verification_res2net.yaml
 #  CUDA_VISIBLE_DEVICES=0 python speaker_verification_plda.py SpeakerRec/hparams/verification_plda_ecapa.yaml
 
 
