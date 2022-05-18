453976474ac10a6034c2e4976adbf12d91a1cede
Mac-Yang <874681044@qq.com>
2022-05-18 14:11:34 +0800

    [train] trials mix pre

--- recipes/CnCeleb1/run.sh
+++ recipes/CnCeleb1/run.sh
@@ -6,7 +6,7 @@
 # time: 2022/3/20 01:05
 # Description: 
 
-stage=30
+stage=0
 
 waited=0
 while [ $(ps 217457 | wc -l) -eq 2 ]; do
@@ -17,9 +17,9 @@ done
 
 if [ $stage -le 0 ]; then
 
-#  CUDA_VISIBLE_DEVICES=1,2 python -m torch.distributed.launch --nproc_per_node=2 --master_port='29501' train_speaker_embeddings.py SpeakerRec/hparams/train_ecapa_tdnn.yaml --distributed_launch --distributed_backend='nccl'
+  CUDA_VISIBLE_DEVICES=1,2 python -m torch.distributed.launch --nproc_per_node=2 --master_port='29501' train_speaker_embeddings.py SpeakerRec/hparams/train_ecapa_tdnn.yaml --distributed_launch --distributed_backend='nccl' --auto_mix_prec
 
-  CUDA_VISIBLE_DEVICES=5 python speaker_verification_cosine.py SpeakerRec/hparams/verification_ecapa.yaml
+#  CUDA_VISIBLE_DEVICES=5 python speaker_verification_cosine.py SpeakerRec/hparams/verification_ecapa.yaml
 #  CUDA_VISIBLE_DEVICES=0 python speaker_verification_plda.py SpeakerRec/hparams/verification_plda_ecapa.yaml
 
   exit
