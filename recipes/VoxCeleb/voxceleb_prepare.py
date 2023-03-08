fa19a1d7db34362f3d4241008bb72694d3d297ea
Wenhao-Yang A315 <874681044@qq.com>
2023-03-08 17:22:47 +0800

    [train] v2

--- recipes/VoxCeleb/voxceleb_prepare.py
+++ recipes/VoxCeleb/voxceleb_prepare.py
@@ -25,6 +25,8 @@ from speechbrain.dataio.dataio import (
 
 logger = logging.getLogger(__name__)
 OPT_FILE = "opt_voxceleb_prepare.pkl"
+WAV_FILE = "wav.scp"
+DUR_FILE = "utt2dur"
 TRAIN_CSV = "train.csv"
 DEV_CSV = "dev.csv"
 TEST_CSV = "test.csv"
@@ -134,23 +136,43 @@ def prepare_voxceleb(
         data_folder = [data_folder]
 
     # _check_voxceleb1_folders(data_folder, splits)
-
     msg = "\tCreating csv file for the VoxCeleb Dataset.."
     logger.info(msg)
 
     # Split data into 90% train and 10% validation (verification split)
-    wav_lst_train, wav_lst_dev = _get_utt_split_lists(
-        data_folder, split_ratio, verification_pairs_file, split_speaker
-    )
+    if os.path.exists(os.path.join(save_folder, WAV_FILE)):
+        print(' => create lists from kaldi scripts')
+        wav_lst_train, wav_lst_dev = _get_utt_split_lists_fromscp(
+            save_folder, split_ratio, verification_pairs_file,
+        )
+    else:
+        print(' => create lists from dirs')
+        wav_lst_train, wav_lst_dev = _get_utt_split_lists(
+            data_folder, split_ratio, verification_pairs_file, split_speaker
+        )
 
     # Creating csv file for training data
     if "train" in splits:
-        prepare_csv(
-            seg_dur, wav_lst_train, save_csv_train, random_segment, amp_th
-        )
+        if os.path.exists(os.path.join(save_folder, DUR_FILE)):
+            utt2dur_file = os.path.join(save_folder, DUR_FILE)
+            prepare_csv_fromscp(
+                utt2dur_file,
+                seg_dur, wav_lst_train, save_csv_train, random_segment, amp_th
+            )
+        else:
+            prepare_csv(
+                seg_dur, wav_lst_train, save_csv_train, random_segment, amp_th
+            )
 
     if "dev" in splits:
-        prepare_csv(seg_dur, wav_lst_dev, save_csv_dev, random_segment, amp_th)
+        if os.path.exists(os.path.join(save_folder, DUR_FILE)):
+            utt2dur_file = os.path.join(save_folder, DUR_FILE)
+            prepare_csv_fromscp(
+                utt2dur_file,
+                seg_dur, wav_lst_dev, save_csv_dev, random_segment, amp_th
+            )
+        else:
+            prepare_csv(seg_dur, wav_lst_dev, save_csv_dev, random_segment, amp_th)
 
     # For PLDA verification
     if "test" in splits:
@@ -379,6 +401,55 @@ def _get_utt_split_lists(
     return train_lst, dev_lst
 
 
+def _get_utt_split_lists_fromscp(save_folder, split_ratio, verification_pairs_file):
+    """
+    Tot. number of speakers vox1= 1211. Tot. number of speakers vox2= 5994.
+    Splits the audio file list into train and dev.
+    This function automatically removes verification test files from the training and dev set (if any).
+    """
+    test_spks = set([])
+    if os.path.exists(verification_pairs_file):
+        for snt in open(verification_pairs_file).readlines():
+            _, enroll_path, eval_path = snt.split()
+            test_spks.add(enroll_path.split('/')[0])
+            test_spks.add(eval_path.split('/')[0])
+        print('There are %d spks in test trials!' % (len(test_spks)))
+
+    # print("Getting file list...")
+    audio_files_dict = {}
+
+    with open(os.path.join(save_folder, WAV_FILE), 'r') as f:
+        for l in f.readlines():
+            uid, upath = l.split()
+            spk_id = uid.aplit('-')[0]
+            if spk_id not in test_spks:
+                audio_files_dict.setdefault(spk_id, []).append(upath)
+
+    train_lst, dev_lst = [], []
+    print('There are %d spks in train set!' % (len(audio_files_dict)))
+    train_spk = set()
+    dev_spk = set()
+    for spk_id in audio_files_dict:
+        spk_id_utts = audio_files_dict[spk_id]
+        random.shuffle(spk_id_utts)
+        train_split = int(max(np.ceil(0.01 * split_ratio[0] * len(spk_id_utts)), 1))
+
+        for i in range(train_split):
+            train_lst.append(spk_id_utts.pop())
+            train_spk.add(spk_id)
+
+        for utts in spk_id_utts:
+            dev_lst.append(utts)
+            dev_spk.add(spk_id)
+
+    print('Split %d spks\'utterances for training and %d spks\'utterances for dev.' % (
+        len(train_spk), len(dev_spk)))
+
+    random.shuffle(train_lst)
+
+    return train_lst, dev_lst
+
+
 def _get_chunks(seg_dur, audio_id, audio_duration):
     """
     Returns list of chunks
@@ -571,6 +642,63 @@ def prepare_csv(seg_dur, wav_lst, csv_file, random_segment=False, amp_th=0):
         entry.append(e_queue.get())
 
     csv_output = csv_output + entry
+
+    # Writing the csv lines
+    with open(csv_file, mode="w") as csv_f:
+        csv_writer = csv.writer(
+            csv_f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
+        for line in csv_output:
+            csv_writer.writerow(line)
+
+    # Final prints
+    msg = "\t%s successfully created!" % (csv_file)
+    logger.info(msg)
+
+def prepare_csv_fromscp(utt2dur_file,
+                        seg_dur, wav_lst, csv_file, random_segment=False, amp_th=0):
+    """
+    Creates the csv file given a list of wav files with utt2dur
+
+    Arguments
+    ---------
+    wav_lst : list
+        The list of wav files of a given data split.
+    csv_file : str
+        The path of the output csv file
+    random_segment: bool
+        Read random segments
+    amp_th: float
+        Threshold on the average amplitude on the chunk.
+        If under this threshold, the chunk is discarded.
+
+    Returns
+    -------
+    None
+    """
+    msg = '\t"Creating csv lists in  %s..."' % (csv_file)
+    logger.info(msg)
+    # print(os.getpid(), " main process")
+    uid2dur = {}
+    with open(utt2dur_file, 'r') as f:
+        for l in f.readlines():
+            uid, d = l.split()
+            uid2dur[uid] = float(d)
+
+    csv_output = [["ID", "duration", "wav", "start", "stop", "spk_id"]]
+    for wav in wav_lst:
+        [spk_id, sess_id, utt_id] = wav.split("/")[-3:]
+        audio_id = '--'.join([spk_id, sess_id, utt_id.split(".")[0]])
+        uid = '-'.join([spk_id, sess_id, utt_id.split(".")[0]])
+
+        audio_duration = uid2dur[uid]
+        uniq_chunks_list = _get_chunks(seg_dur, audio_id, audio_duration)
+        for chunk in uniq_chunks_list:
+                s, e = chunk.split("_")[-2:]
+                start_sample = int(float(s) * SAMPLERATE)
+                end_sample = int(float(e) * SAMPLERATE)
+
+                csv_output.append([chunk, str(audio_duration), wav, start_sample, end_sample, spk_id])
 
     # Writing the csv lines
     with open(csv_file, mode="w") as csv_f:
